f715798a522b6ea91202a59b1f1291db51d62507
Poggu <poggu@seznam.cz>
2024-09-02 20:50:06 +0200

    test

--- xmake.lua
+++ xmake.lua
@@ -1,6 +1,6 @@
 add_rules("mode.debug", "mode.release")
 add_requires("nlohmann_json", "tabulate")
-add_requires("libcurl", {configs = {openssl = true}})
+add_requires("libcurl", {configs = {mbedtls = true}})
 
 target("PlayerFetch")
 	set_kind("binary")
