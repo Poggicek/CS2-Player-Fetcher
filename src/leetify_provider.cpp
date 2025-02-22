9fb02a9749108abeca053305b41645fce01d1117
Pavel Djundik <xPaw@users.noreply.github.com>
2025-02-22 13:48:36 +0200

    ssl verification should work

--- src/leetify_provider.cpp
+++ src/leetify_provider.cpp
@@ -26,8 +26,6 @@ LeetifyUser GetLeetifyUser(CSteamID steamID)
 		                 "CS2 Player Fetcher (+https://github.com/Poggicek/CS2-Player-Fetcher)");
 		curl_easy_setopt(curl, CURLOPT_TIMEOUT, 60L);
 		curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
-		curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0);
-		curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0);
 		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
 		curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
 
